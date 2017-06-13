bdd734d539e7ee4829f3c74c1b86e69188bb61b5
juj7347 <juj7347@naver.com>
2017-06-13 22:31:03 +0900

    Create 과제4.c

--- 과제4.c
+++ 과제4.c
@@ -1,278 +1,353 @@
+#define BLACK 1
+#define RED 0
 #include <stdio.h>
 #include <stdlib.h>
-#define Black 1
-#define Red 0
-
-
 int bh = 0;
-int tb = 0;
-int tr = 0;
 int total = 0;
-int deleted = 0;
-int misscount = 0;
+int del = 0;
+int insert = 0;
+int miss = 0;
 
 typedef struct Node*NodePtr;
-struct Node {
+struct Node{
 	int val;
-	NodePtr parent, left, right;
-	int color;
+	NodePtr p, left, right;
+	int c;
 };
 
+NodePtr nil;
 
-NodePtr node_alloc(int newval) {
+NodePtr node_alloc(int newval){
 	NodePtr self = (NodePtr)malloc(sizeof(struct Node));
 	self->val = newval;
-	self->parent = NULL;
-	self->left = NULL;
-	self->right = NULL;
-	self->color = Black;
+	self->p = nil;
+	self->left = nil;
+	self->right = nil;
 	return self;
 }
 
+NodePtr nil_init(){
+	nil = (NodePtr)malloc(sizeof(struct Node));
+	nil->val = -1;
+	nil->c = 1;
+	nil->left = nil->right = NULL;
 
+	return nil;
+
+}
 typedef struct RBT* RBTPtr;
-struct RBT {
-	NodePtr root;
+struct RBT{
+	NodePtr root, NIL;
 };
 
 
-RBTPtr rbt_alloc() {
+RBTPtr rbt_alloc(){
 	RBTPtr self = (RBTPtr)malloc(sizeof(struct RBT));
-	self->root = NULL;
+	self->NIL = nil;
+	self->root = self->NIL;
 	return self;
 }
 
 
-void Left_Rotate(RBTPtr self, NodePtr n) {
+void Left_Rotate(RBTPtr self, NodePtr n){
 	NodePtr y;
 	y = n->right;
 	n->right = y->left;
-	if (y->left != NULL)
-		y->left->parent = n;
-	y->parent = n->parent;
-	if (n->parent == NULL)
+	if (y->left != self->NIL)
+		y->left->p = n;
+	y->p = n->p;
+	if (n->p == self->NIL)
 		self->root = y;
-	else if (n == n->parent->left)
-		n->parent->left = y;
+	else if (n == n->p->left)
+		n->p->left = y;
 	else
-		n->parent->right = y;
+		n->p->right = y;
 	y->left = n;
-	n->parent = y;
+	n->p = y;
 }
 
 
-void Right_Rotate(RBTPtr self, NodePtr n) {
+void Right_Rotate(RBTPtr self, NodePtr n){
 	NodePtr y;
 	y = n->left;
 	n->left = y->right;
-	if (y->right != NULL)
-		y->right->parent = n;
-	y->parent = n->parent;
-	if (n->parent == NULL)
+	if (y->right != self->NIL)
+		y->right->p = n;
+	y->p = n->p;
+	if (n->p == self->NIL)
 		self->root = y;
-	else if (n == n->parent->right)
-		n->parent->right = y;
+	else if (n == n->p->left)
+		n->p->left = y;
 	else
-		n->parent->left = y;
+		n->p->right = y;
 	y->right = n;
-	n->parent = y;
+	n->p = y;
 }
 
 
-void rbt_insert_fixup(RBTPtr self, NodePtr n) {
+void rbt_insert_fixup(RBTPtr self, NodePtr n){
 	NodePtr y;
-	while (n->parent != NULL && n->parent->color == Red) {
-		if (n->parent == n->parent->parent->left) {
-			y = n->parent->parent->right;
-			if (y != NULL && y->color == Red) {
-				n->parent->color = Black;
-				y->color = Black;
-				n->parent->parent->color = Red;
-				n = n->parent->parent;
+	while (n->p->c == RED){
+		if (n->p == n->p->p->left){
+			y = n->p->p->right;
+			if (y != self->NIL && y->c == RED){
+				n->p->c = BLACK;
+				y->c = BLACK;
+				n->p->p->c = RED;
+				n = n->p->p;
 			}
-			else if (n == n->parent->right) {
-				n = n->parent;
-				Left_Rotate(self, n);
-			}
-			else {
-				n->parent->color = Black;
-				n->parent->parent->color = Red;
-				Right_Rotate(self, n->parent->parent);
+			else{
+				if (n == n->p->right){
+					n = n->p;
+					Left_Rotate(self, n);
+				}
+				n->p->c = BLACK;
+				n->p->p->c = RED;
+				Right_Rotate(self, n->p->p);
 			}
 		}
-		else if (n->parent == n->parent->parent->right) {
-			y = n->parent->parent->left;
-			if (y != NULL && y->color == Red) {
-				n->parent->color = Black;
-				y->color = Black;
-				n->parent->parent->color = Red;
-				n = n->parent->parent;
+		else{
+			y = n->p->p->left;
+			if (y->c == RED){
+				n->p->c = BLACK;
+				y->c = BLACK;
+				n->p->p->c = RED;
+				n = n->p->p;
 			}
-			else if (n == n->parent->left) {
-				n = n->parent;
-				Right_Rotate(self, n);
-			}
-			else {
-				n->parent->color = Black;
-				n->parent->parent->color = Red;
-				Left_Rotate(self, n->parent->parent);
+			else{
+				if (n == n->p->left){
+					n = n->p;
+					Right_Rotate(self, n);
+				}
+				n->p->c = BLACK;
+				n->p->p->c = RED;
+				Left_Rotate(self, n->p->p);
 			}
 		}
 	}
-	self->root->color = Black;
+	self->root->c = BLACK;
 }
 
 
-void rbt_insert(RBTPtr self, NodePtr n) {
-	NodePtr y = NULL;
+void rbt_insert(RBTPtr self, NodePtr n){
+	NodePtr y = self->NIL;
 	NodePtr x = self->root;
-	while (x != NULL) {
+	while (x != self->NIL){
 		y = x;
 		if (n->val < x->val)
 			x = x->left;
 		else
 			x = x->right;
 	}
-	n->parent = y;
-	if (y == NULL)
+	n->p = y;
+	if (y == self->NIL)
 		self->root = n;
 	else if (n->val < y->val)
 		y->left = n;
 	else
 		y->right = n;
-	n->left = NULL;
-	n->right = NULL;
-	n->color = Red;
+	n->left = self->NIL;
+	n->right = self->NIL;
+	n->c = RED;
 	rbt_insert_fixup(self, n);
+	insert++;
+}
+void rbt_transplant(RBTPtr self, NodePtr u, NodePtr v){
+	if (u->p == self->NIL)
+		self->root = v;
+	else if (u == u->p->left)
+		u->p->left = v;
+	else
+		u->p->right = v;
+	v->p = u->p;
+}
+NodePtr tree_min(RBTPtr self, NodePtr x){
+	while (x->left != self->NIL)
+		x = x->left;
+	return x;
+}
+void rbt_delete_fixup(RBTPtr self, NodePtr n){
+	NodePtr w;
+	while (n != self->root && n->c == BLACK){
+		if (n == n->p->left){
+			w = n->p->right;
+			if (w->c == RED){
+				w->c = BLACK;
+				n->p->c = RED;
+				Left_Rotate(self, n->p);
+				w = n->p->right;
+			}
+			if (w->left->c == BLACK && w->right->c == BLACK){
+				w->c = RED;
+				n = n->p;
+			}
+			else{
+				if (w->right->c == BLACK){
+					w->left->c = BLACK;
+					w->c = RED;
+					Right_Rotate(self, w);
+					w = n->p->right;
+				}
+				w->c = n->p->c;
+				n->p->c = BLACK;
+				w->right->c = BLACK;
+				Left_Rotate(self, n->p);
+				n = self->root;
+			}
+		}
+		else{
+			w = n->p->left;
+			if (w->c == RED){
+				w->c = BLACK;
+				n->p->c = RED;
+				Right_Rotate(self, n->p);
+				w = n->p->left;
+			}
+			if (w->right->c == BLACK && w->left->c == BLACK){
+				w->c = RED;
+				n = n->p;
+			}
+			else{
+				if (w->left->c == BLACK){
+					w->right->c = BLACK;
+					w->c = RED;
+					Left_Rotate(self, w);
+					w = n->p->left;
+				}
+				w->c = n->p->c;
+				n->p->c = BLACK;
+				w->left->c = BLACK;
+				Right_Rotate(self, n->p);
+				n = self->root;
+			}
+		}
+
+	}
+	n->c = BLACK;
+}
+void rbt_delete(RBTPtr self, NodePtr n){
+	NodePtr y, x;
+	y = n;
+	int y_original_color = y->c;
+	if (n->left == self->NIL){
+		x = n->right;
+		rbt_transplant(self, n, n->right);
+	}
+	else if (n->right == self->NIL){
+		x = n->left;
+		rbt_transplant(self, n, n->left);
+	}
+	else{
+		y = tree_min(self, n->right);
+		y_original_color = y->c;
+		x = y->right;
+		if (y->p == n)
+			x->p = y;
+		else{
+			rbt_transplant(self, y, y->right);
+			y->right = n->right;
+			y->right->p = y;
+		}
+		rbt_transplant(self, n, y);
+		y->left = n->left;
+		y->left->p = y;
+		y->c = n->c;
+	}
+	if (y_original_color == BLACK)
+		rbt_delete_fixup(self, x);
 }
 
-void rbt_print(RBTPtr self, NodePtr tree, int level) {
-	if (tree->right != NULL)
+void rbt_print(RBTPtr self, NodePtr tree, int level){
+	if (tree->right != self->NIL)
 		rbt_print(self, tree->right, level + 1);
 	for (int i = 0; i < level; i++)
-		printf("    ");
-	if (tree->color == Black) {
-		printf("%dB\n", tree->val);
-		tb = tb + 1;
-	}
-	if (tree->color == Red) {
-		printf("%dR\n", tree->val);
-		tr = tr + 1;
-	}
-	if (tree->left != NULL)
+		printf("   ");
+	printf("%d-%s\n", tree->val, !(tree->c) ? "R" : "B");
+	if (tree->left != self->NIL)
 		rbt_print(self, tree->left, level + 1);
 }
 
-struct node
-{
-	int data;
-	struct node *next;
-};
-void push(struct node** head_ref, int new_data)
-{
-	struct node* new_node = (struct node*) malloc(sizeof(struct node));
-	new_node->data = new_data;
-	new_node->next = (*head_ref);
-	(*head_ref) = new_node;
+int rbt_count(RBTPtr self, NodePtr tree){
+	if (tree == self->NIL)
+		return 0;
+	if (tree->left != self->NIL)
+		return rbt_count(self, tree->left) + rbt_count(self, tree->right) + 1;
+	else
+		return rbt_count(self, tree->left) + rbt_count(self, tree->right) ;
 }
 
-void deleteNode(struct node **head_ref, int key)
-{
-	struct node* tmp = *head_ref, *prev;
-	if (tmp != NULL && tmp->data == key)
-	{
-		*head_ref = tmp->next;
-		free(tmp);
-		
+
+void rbt_bh(RBTPtr self, NodePtr tree){
+	if (tree == self->NIL){
 		return;
 	}
-
-
-
-	while (tmp != NULL && tmp->data != key)
-	{
-		prev = tmp;
-		tmp = tmp->next;
-	}
-
-	if (tmp == NULL) {
-		misscount++;
-		return; }
-
-	prev->next = tmp->next;
-
-	free(tmp);
-}
-
-void rblist(struct node *node)
-{
-	while (node != NULL)
-	{
-		printf(" %d ", node->data);
-		node = node->next;
-	}
-}
-
-int rbt_bh(RBTPtr self, NodePtr tree, int bh) {
-	if (tree == NULL) {
-		return bh;
-	}
-	else {
-		if (tree->color == Black)
-			rbt_bh(self, tree->right, bh + 1);
-		else if (tree->color == Red)
-			rbt_bh(self, tree->right, bh);
-	}
-}
-
-void inorder_traversal(NodePtr root) {
-	if (root != NULL) {
-		inorder_traversal(root->left);
-		if (root->color == 1) {
-			printf("%dB  ", root->val);
+	else{
+		if (tree->c == BLACK) {
+			bh++;
+			rbt_bh(self, tree->right);
 		}
-		else if (root->color == 0) {
-			printf("%dR  ", root->val);
-		}
-		inorder_traversal(root->right);
+		else if (tree->c == RED)
+			rbt_bh(self, tree->right);
 	}
 }
 
-int main()
-{
-	struct node* head = NULL;
-	struct node* c = NULL;
-	FILE *fp;
-	int data;
+
+NodePtr rbt_search(RBTPtr self, NodePtr x, int val){
+	if (x == self->NIL || x->val == val) 
+		return x;
+	else if (val <= x->val)
+		return rbt_search(self, x->left, val);
+	else
+		return rbt_search(self, x->right, val);
+}
+void inorder_traversal(RBTPtr self, NodePtr root) {
+	if (root != self->NIL) {
+		inorder_traversal(self, root->left);
+		printf("%d %s\n", root->val, !(root->c) ? "R" : "B");
+		inorder_traversal(self, root->right);
+	}
+}
+void total_counter(RBTPtr self, NodePtr root) {
+	if (root != self->NIL) {
+		total_counter(self, root->left);
+		total++;
+		total_counter(self, root->right);
+	}
+}
+
+int main(){
+	nil = nil_init();
 	RBTPtr rbt = rbt_alloc();
-	fp = fopen("C:\\Users\\정윤재\\Desktop\\자구\\input6.txt", "r");
-	while (!feof(fp)) {
-		fscanf(fp, "%d", &data);
-		if (data >0)
-			push(&head, data);
-		else if (data < 0) {
-			deleteNode(&head, -1 * data);
-			deleted++;
+	FILE *fp;
+	int num;
+	fp = fopen("C:\\Users\\정윤재\\Downloads\\rbtest\\test01.txt", "r");
+	while (!feof(fp)){
+		fscanf(fp, "%d", &num);
+		if (num > 0){
+			rbt_insert(rbt, node_alloc(num));
 		}
-		else if (data == 0) {
+		else if (num < 0){
+			NodePtr node1 = rbt_search(rbt, rbt->root, num*-1);
+			if (node1->val != -1){
+				rbt_delete(rbt, node1);
+				del++;
+			}
+			else
+				miss++;
+		}
+		else if (num == 0)
 			break;
-		}
-	}
-	c = head;
-	while (c != NULL)
-	{
-		rbt_insert(rbt, node_alloc(c->data));
-		c = c->next;
-
 	}
 	fclose(fp);
-	rbt_print(rbt, rbt->root, 0);
-	printf("\n\n\n\n");
-	printf("bh is %d\n", rbt_bh(rbt, rbt->root, bh));
-	printf("total is %d\n", tb + tr);
-	printf("nb is %d\n\n\n", tb);
-	printf("deleted = %d\n\n\n", deleted - misscount);
-	printf("miss = %d\n\n\n", misscount);
-	inorder_traversal(rbt->root);
+	rbt_bh(rbt, rbt->root);
+	total_counter(rbt, rbt->root);
+	//rbt_print(rbt, rbt->root, 0); 트리의 모양을 출력해주는 함수
+	printf("nb = %d\n", rbt_count(rbt, rbt->root));
+	printf("total = %d\n", total);
+	printf("bh = %d\n", bh);
+	printf("del = %d\n", del);
+	printf("miss = %d\n", miss);
+	printf("insert = %d\n", insert);
+	inorder_traversal(rbt, rbt->root);
 	return 0;
 }
